4f5165b6019edbe8a375f523c5aa63437511fd68
Mark Allan B. Meriales <markallanb.meriales@Marks-MacBook-Pro.local>
2017-08-16 19:56:23 +0800

    fix java package

--- android/src/main/java/com/reactnativecallhistory/RNCallHistoryPackage.java
+++ android/src/main/java/com/reactnativecallhistory/RNCallHistoryPackage.java
@@ -5,28 +5,25 @@ import android.app.Activity;
 import java.util.*;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 public class RNCallHistoryPackage implements ReactPackage {
-    private RNCallHistoryPackage mModuleInstance;
-
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      mModuleInstance = new RNCallHistoryPackage(reactContext);
-      return Arrays<NativeModule>asList(mModuleInstance);
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(
+    ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new ToastModule(reactContext));
+
+        return modules;
     }
 
 }
